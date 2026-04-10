f49d7d14f2bbf26a3c85f823a2c7fcb699a72514
Philip Bedrosian <philipbed52@gmail.com>
2026-04-09 23:45:16 -0700

    AHHHHH

--- build/_app/immutable/chunks/CEr1hXzh.js
+++
@@ -1,1 +0,0 @@
-async function n(){const t=await fetch("/api/get_event");if(!t.ok)throw new Error(`Request failed with status ${t.status}: ${t.statusText}`);return(await t.json()).message}export{n as g};
